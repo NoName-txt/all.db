eb7ea2b2aef386bff316cef51d392aec8ca7a569
NoName.txt <79569774+NoName-txt@users.noreply.github.com>
2022-02-03 23:02:22 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,131 @@
+# all.db
+
+If you found a bug contact me on [Discord](https://discord.com/users/360322989515866112).
+
+## Set
+```js
+const data = require("all.db");
+const db = new data({dataPath:"./data.json"});
+
+db.set("nonametxt.test","Developer");
+```
+Output:
+```json
+{
+  "nonametxt":{
+    "test":"Developer"
+  }
+}
+```
+---
+## Delete
+```js
+const data = require("all.db");
+const db = new data({dataPath:"./data.json"});
+
+db.delete("nonametxt.test");
+db.remove("nonametxt.test");
+```
+Output:
+```json
+{
+}
+```
+---
+## Add
+```js
+const data = require("all.db");
+const db = new data({dataPath:"./data.json"});
+
+db.add("nonametxt.number",1);
+```
+Output:
+```json
+1
+```
+---
+## Push
+```js
+const data = require("all.db");
+const db = new data({dataPath:"./data.json"});
+
+db.push("nonametxt.array",{name:"NoNametxt"});
+
+//If it's string before it replaces it hardly
+db.push("nonametxt.array",{name:"NoNametxt"},true);
+```
+Output:
+```json
+{
+  "nonametxt":{
+    "array":[
+      {
+        "name":"NoNametxt"
+      }
+    ]
+  }
+}
+```
+---
+## Pull
+```js
+const data = require("all.db");
+const db = new data({dataPath:"./data.json"});
+
+db.pull("nonametxt.array","NoNametxt","name");
+
+//If straight array
+db.pull("nonametxt.array","NoNametxt");
+```
+Output:
+```json
+{
+  "nonametxt":{
+    "array":[]
+  }
+}
+```
+---
+## Get
+```js
+const data = require("all.db");
+const db = new data({dataPath:"./data.json"});
+
+db.get("nonametxt");
+db.fetch("nonametxt");
+```
+Output:
+```json
+"Developer"
+```
+---
+## Data Exists
+```js
+const data = require("all.db");
+const db = new data({dataPath:"./data.json"});
+
+db.exists("nonametxt.test");
+db.has("nonametxt.test");
+```
+Output:
+```js
+true or false
+```
+---
+## Get All
+```js
+const data = require("all.db");
+const db = new data({dataPath:"./data.json"});
+
+db.getAll("nonametxt");
+```
+Output:
+```
+Shows the direct file
+```
+---
+
+Thx For Use [all.db](https://www.npmjs.com/package/all.db).
+
+[Discord](https://discord.com/users/360322989515866112).
+
---
+++ all.js
@@ -0,0 +1,115 @@
+const fs = require("fs");
+
+class Database{
+    constructor({dataPath="./data.json"} = {}){
+        this.path = dataPath;
+        if (!fs.existsSync(dataPath)) {
+            console.log("Database file created \nPath: " + this.path);
+            fs.writeFileSync(dataPath,"{}", { flag: 'wx' })
+        }
+        this.readFile = () => JSON.parse(fs.readFileSync(dataPath, "utf8"));
+        this.writeFile = (data) => fs.writeFileSync(dataPath, JSON.stringify(data, null, 2));
+        
+    }    
+    
+    set(info, is) {
+        const data = this.readFile();
+
+        const text = info.toString()
+
+        function setValue(obj, path, value) {
+            var a = path.split(".");
+            var o = obj
+            while (a.length - 1) {
+                var n = a.shift();
+                if (!(n in o)) o[n] = {}
+                o = o[n]
+            }
+            o[a[0]] = value
+        }
+        
+        setValue(data, text, is);
+        this.writeFile(data);
+        return this.get(info);
+
+    }
+
+    delete(info) {
+        return this.set(info,undefined);
+    }
+
+    remove(info) {
+        return this.delete(info);
+    }
+    
+    get(info) {
+        const data = this.readFile();
+        const text = info.toString();
+
+        function getValue(obj, path) {
+            var name = path.split(".");
+            while (name.length - 1) {
+                var nshift = name.shift();
+                if (!(nshift in obj)) obj[nshift] = {}
+                obj = obj[nshift]
+            }
+            return obj[name[0]]
+        }
+
+        return getValue(data, text);
+    }
+
+    fetch(info){
+        return this.get(info)
+    }
+
+
+    has(info){
+        const data = this.get(info);
+        if(data === undefined) return false; 
+            else return true;
+    }
+
+    exists(info){
+        return this.has(info);
+    }
+
+    add(info,number){
+        const data = this.get(info);
+        if(isNaN(data) && isNaN(number)) throw new Error("This is not a number");
+        var newNumber = data + number;
+        return this.set(info,newNumber);
+    }
+
+    push(info, value, hardly) {
+        const data = this.get(info);
+        if (Array.isArray(data)) {
+            var arr = data || [];
+            arr.push(value);
+            return this.set(info,arr);
+        } else {
+            if(hardly != true && typeof data.toLowerCase() === "string"){ 
+                throw new Error("This is not an array");
+            }
+            return this.set(info,[value]);
+        }
+    }
+
+    pull(info, text, id) {
+        const data = this.get(info);
+        var focusIndex = data.map(dt => {
+            if(id) return dt[id]; else return dt;
+        }).indexOf(text);
+
+        if (focusIndex !== -1) data.splice(focusIndex, 1);
+        return this.set(info,data);
+    }
+
+    getAll() {
+        let data = this.readFile();
+        return data;
+    }
+  
+  }
+
+  module.exports = Database;
